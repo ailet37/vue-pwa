d156f3af535fad643b1a9db7f475e9ed92d50980
alina <140783>
2019-02-26 16:53:36 +0300

    init

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDYl9HCn19rJ51Nn-xjBcMZ5qccy-a8ySQ",
+    authDomain: "vue-pwa-82f11.firebaseapp.com",
+    databaseURL: "https://vue-pwa-82f11.firebaseio.com",
+    projectId: "vue-pwa-82f11",
+    storageBucket: "vue-pwa-82f11.appspot.com",
+    messagingSenderId: "526403485430"
+  };
+firebase.initializeApp(config);
